dd6db6be984477bbaf43b90b79a37974b5a03299
Erikinol <70723194+Erikinol@users.noreply.github.com>
2021-03-01 15:24:14 +0100

    Update load_data.py
    Remove rows with "?" data

--- code/load_data.py
+++ code/load_data.py
@@ -3,10 +3,18 @@ import pandas as pd
 
 # n.b. paths are set as if executing from top repo dir, NOT the code dir
 filename_attributes = "../data/imports-85.labels.txt"
-filename_data = "../data/imports-85.data.txt"
 
+#Specify path to file with attribute headers
+filename_data = "../data/imports-85.data.txt"
 names = np.loadtxt(filename_attributes, dtype=str)
 
+#Read data into pandas data frame
 raw_data = pd.read_csv(filename_data, names=names)
 
+#Remove column "normalized-losses"
 raw_data = raw_data.drop(['normalized-losses'], axis=1)
+
+#Remove all rows with "?" as given value
+raw_data = raw_data.replace('?', np.NaN)
+raw_data = raw_data.dropna(axis=0)
+
