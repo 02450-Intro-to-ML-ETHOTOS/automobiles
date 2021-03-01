202a85e6f68a0c43223be48717e6b2b125741cf1
tstannius <tobiasostannius@gmail.com>
2021-03-01 12:25:24 +0100

    Add load data example

---
+++ code/load_data.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+# n.b. paths are set as if executing from top repo dir, NOT the code dir
+filename_attributes = "data/imports-85.labels.txt"
+filename_data = "data/imports-85.data.txt"
+
+names = np.loadtxt(filename_attributes, dtype=str)
+
+df = pd.read_csv(filename_data, names=names)
+print(df.head())
