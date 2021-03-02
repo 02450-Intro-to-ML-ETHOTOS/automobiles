6dfe1e404c1bbf41502ccdb70ac78eabe16b2c6a
Erikinol <70723194+Erikinol@users.noreply.github.com>
2021-03-02 16:16:02 +0100

    Variable name change

---
+++ code/pca.py
@@ -0,0 +1,8 @@
+from load_data import *
+
+from matplotlib.pyplot import figure, plot, title, legend, xlabel, ylabel, show
+
+#Extract attributes from 
+attributeNames = doc.row_values(0, 3, 11)
+
+
