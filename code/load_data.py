779c9c6f2fab4680baa308beede4348c96324515
Erikinol <70723194+Erikinol@users.noreply.github.com>
2021-03-03 15:58:36 +0100

    Change of import names
    Jeg har ændret header navne fordi jeg haft problem med "-"

--- code/load_data.py
+++ code/load_data.py
@@ -6,13 +6,13 @@ filename_attributes = "../data/imports-85.labels.txt"
 
 #Specify path to file with attribute headers
 filename_data = "../data/imports-85.data.txt"
-names = np.loadtxt(filename_attributes, dtype=str)
+names = np.loadtxt(filename_attributes, dtype=object)
 
 #Read data into pandas data frame
 raw_data = pd.read_csv(filename_data, names=names)
 
 #Remove column "normalized-losses"
-raw_data = raw_data.drop(['normalized-losses'], axis=1)
+raw_data = raw_data.drop(['normalized_losses'], axis=1)
 
 #Remove all rows with "?" as given value
 raw_data = raw_data.replace('?', np.NaN)
@@ -21,3 +21,4 @@ raw_data = raw_data.dropna(axis=0)
 # Cast numeric values to float
 # see https://stackoverflow.com/a/16134561/13962373
 raw_data = raw_data.apply(pd.to_numeric, errors='ignore')
+
--- code/pca.py
+++ code/pca.py
@@ -1,6 +1,32 @@
 from load_data import *
 
-from matplotlib.pyplot import figure, plot, title, legend, xlabel, ylabel, show
+from scipy.linalg import svd
 
-#Extract attributes 
-attributeNames = data["wheel-base", "length"]
+classLabels= raw_data.body_style.tolist()
+X = raw_data.drop(['body_style', 'engine_type', 'num_of_cylinders', 'fuel_system',
+                         'symboling', 'make',  'fuel_type', 'aspiration', 'num_of_doors',
+                         'engine_location', 'drive_wheels'], axis=1)
+ 
+#Extract list with attributeNames
+attributeNames = np.array(X.columns)
+
+# Extract class names to python list,
+# then encode with integers (dict)
+classNames = sorted(set(classLabels))
+classDict = dict(zip(classNames, range(5)))
+
+# Extract vector y, convert to NumPy array
+y = np.asarray([classDict[value] for value in classLabels])
+
+# Compute values of N, M and C.
+N = len(y)
+M = len(attributeNames)
+C = len(classNames)
+
+# Subtract mean value from data
+X = np.array(X)
+Y = X - np.ones((N,1))*X.mean(axis=0)
+
+# PCA by computing SVD of Y
+U,S,V = svd(Y,full_matrices=False)
+
