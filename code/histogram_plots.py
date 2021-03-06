468788b3bde975d3a01c24b931f988c96f227ab6
tstannius <tobiasostannius@gmail.com>
2021-03-06 18:37:30 +0100

    Finalize histograms

--- code/histogram_plots.py
+++ code/histogram_plots.py
@@ -1,13 +1,5 @@
 from load_data import *
 
-import matplotlib.pyplot as plt
-
-import numpy as np
-import scipy.stats as sps
-
-
-sumstats_numerical_attrs = raw_data.describe()
-numerical_attrs = sumstats_numerical_attrs.columns.values
 
 # subset dataframe to X
 X = raw_data[numerical_attrs].values
@@ -17,16 +9,31 @@ N = X.shape[0] # num obs
 
 
 # Plot histograms
-plt.figure(figsize=(14,9))
+fig = plt.figure(figsize=(16,10))
 u = np.floor(np.sqrt(M)); v = np.ceil(float(M)/u)
 for i in range(M):
     plt.subplot(u,v,i+1)
-    plt.hist(X[:,i])
+    plt.hist(X[:,i], bins=14, color="dodgerblue")
     # plt.xlabel(numerical_attrs[i])
     plt.ylim(0, N) # Make the y-axes equal for improved readability
     if i%v!=0: plt.yticks([])
     plt.title(numerical_attrs[i])
 
-plt.suptitle("Automobiles, numerical attributes. No normalization, no outlier removal")  # 
-plt.show()
 
+# plt.subplots_adjust(wspace=0.3, hspace=0.3)
+plt.suptitle("Distributions of Values for Numerical Attributes", fontsize="xx-large")
+fig.tight_layout()
+# show or save plot
+# plt.show()
+plt.savefig("../out/plots/histograms.png", dpi=200)
+
+
+# histogram of single attribute
+fig = plt.figure()
+plt.hist(X[:,num_attr2idx["peak_rpm"]], bins=50, color="dodgerblue")
+plt.ylim(0, N)
+plt.title("Peak RPM", fontsize="xx-large")
+fig.tight_layout()
+# show or save plot
+# plt.show()
+plt.savefig("../out/plots/histogram_peak_rpm.png", dpi=200)
