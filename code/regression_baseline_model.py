24318b9dff03a94f170bee62c73b5ac80f3847cd
tstannius <tobiasostannius@gmail.com>
2021-04-09 19:39:53 +0200

    Refactor and comment base model

--- code/regression_baseline_model.py
+++ code/regression_baseline_model.py
@@ -5,16 +5,14 @@ from regression_transform_data import *
 class RegressionBaselineModel(object):
     """A simple baseline model which simply predicts the mean of y"""
     def __init__(self):
-        super(RegressionBaselineModel, self).__init__()
         self.y_pred = None
 
     def fit(self, y):
-        # TODO: do we need to do K-fold CV?
+        # TODO: use K-fold CV
         self.y_pred = y.mean()
 
     def predict(self, X):
-        if self.y_pred is None:
-            raise ValueError("Model not trained yet!")
+        assert(self.y_pred is not None), "Model not trained yet!"
         
         y_pred = np.ones(X.shape[0]) * self.y_pred
         return y_pred
