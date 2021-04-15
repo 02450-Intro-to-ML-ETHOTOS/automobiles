99db3533991fb8c4bbbb67e7722620d7466c979b
Erikinol <70723194+Erikinol@users.noreply.github.com>
2021-04-15 11:50:10 +0200

    classification_data_transform

---
+++ code/classification_transform_data.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from regression_transform_data import *
+
+# extract body style columns and then remove them from matrix of 
+# categorical values
+body_style = X_cat[["body_style_hardtop", "body_style_hatchback", "body_style_sedan",
+                    "body_style_wagon"]]
+X_cat = X_cat.drop(body_style, axis=1)
+
+# convert body_style dataframe to numpy array
+y = body_style.to_numpy()
+
+# transform numerical data of price to get mean 0 and unit variance
+X_num = raw_data[numerical_attrs].values
+X_num = X_num - (np.ones((N, 1)) * X_num.mean(axis=0))
+X_num = X_num * (1/np.std(X_num, 0))
+
+# combine numerical and categorical data
+X = np.hstack((X_num, X_cat.values))  # 59 attributes in total
+
+# set up useful variables
+N, M = X.shape
+
+# append numerical attributes header list with price, (now 14 in total)
+numerical_attrs.append("price")
+
+#remove categorical attributes header list with "body_style" (now 10 in total)
+categorical_attrs.remove("body_style")
+
+# list of attribute names
+attributeNames = numerical_attrs + (X_cat.columns.values).tolist()
+
---
+++ code/classification_tree_model.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 08:23:54 2021
+
+@author: Holmen_Olofsson
+"""
+
