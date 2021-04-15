243913c978bab338bd3e11f71a63423c194cff1b
Erikinol <70723194+Erikinol@users.noreply.github.com>
2021-04-15 13:00:40 +0200

    error correction in data_transformation_classification

--- code/classification_transform_data.py
+++ code/classification_transform_data.py
@@ -14,6 +14,13 @@ y = body_style.to_numpy()
 convertible = (1*np.all(y == 0, axis=1)).reshape((-1,1))
 y = np.append(y, convertible, axis=1)
 
+# append numerical attributes header list with price, (now 14 in total)
+numerical_attrs.append("price")
+
+#remove categorical attributes header list with "body_style" (now 10 in total)
+categorical_attrs.remove("body_style")
+
+
 # transform numerical data of price to get mean 0 and unit variance
 X_num = raw_data[numerical_attrs].values
 X_num = X_num - (np.ones((N, 1)) * X_num.mean(axis=0))
@@ -24,13 +31,6 @@ X = np.hstack((X_num, X_cat.values))  # 59 attributes in total
 
 # set up useful variables
 N, M = X.shape
-
-# append numerical attributes header list with price, (now 14 in total)
-numerical_attrs.append("price")
-
-#remove categorical attributes header list with "body_style" (now 10 in total)
-categorical_attrs.remove("body_style")
-
 
 # list of attribute names
 attributeNames = numerical_attrs + (X_cat.columns.values).tolist()
--- code/classification_tree_model.py
+++ code/classification_tree_model.py
@@ -1,1 +1,13 @@
+from classification_transform_data import *
+
+from sklearn import tree
+
+# Fit regression tree classifier, Gini split criterion, no pruning
+criterion='gini'
+dtc = tree.DecisionTreeClassifier(criterion=criterion, min_samples_split=100)
+dtc = dtc.fit(X,y)
+
+fname='tree_' + criterion + '_car_data'
+# Export tree graph .gvz file to parse to graphviz
+out = tree.export_graphviz(dtc, out_file=fname + '.gvz', feature_names=attributeNames)
 
