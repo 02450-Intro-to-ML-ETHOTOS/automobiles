5c96f7b80d2f5f149b3a4fe2f25a120f2878404c
tstannius <tobiasostannius@gmail.com>
2021-04-15 08:39:38 +0200

    Finalize model comparison framework

--- code/regression_model_comparison.py
+++ code/regression_model_comparison.py
@@ -1,26 +1,33 @@
-from regression_transform_data import *
+from regression_transform_data import * # imports numpy as np, pandas as pd, various sklearn modules
 from regression_baseline_model import *
 from regression_regularized_model import *
-# imports numpy as np, pandas as pd, various sklearn modules
+from regression_ann_model import *
+from t_test_paired import t_test_paired
+
+import torch
 
 from pprint import pprint
 import scipy.stats as st
 from itertools import combinations
 
-# set up cross validation for outer folds
-K = 10
-# we set random_state to ensure reproducibility
-CV = model_selection.KFold(K, shuffle=True, random_state=42)
 
-error_train = np.empty((K,1))
 
 # set up data structures for storing errors and parameters using dict comprehension
 models = ["B", "ANN", "RR"] # baseline, artificial neural network, ridge regression
-model_errors_test = {k: [] for k in models} # this will make a dict with a list for each model
-model_parameters = {k: [] for k in models}
+# this will make a dict with a list for each model to contain the test error for each fold
+model_errors_test = {m: [] for m in models}
+model_parameters = {m: [] for m in models}
 
-error_test = np.empty((K,1))
 
+def evaluate_model(model, X, y):
+    y_pred = model.predict(X)
+    error_squared = np.power((y - y_pred), 2).sum() / X.shape[0]
+    return error_squared
+
+# set up cross validation for outer folds
+K = 2
+# we set random_state to ensure reproducibility
+CV = model_selection.KFold(K, shuffle=True, random_state=42)
 for k, (train_index, test_index) in enumerate(CV.split(X, y)): # use enumerate to get index k
     # Let Dk^train, Dk^test be the k'th split of D
     # extract training and test set for current CV fold
@@ -39,71 +46,46 @@ for k, (train_index, test_index) in enumerate(CV.split(X, y)): # use enumerate t
     # baseline model
     basic_model = RegressionBaselineModel()
     basic_model.fit(y_train)
-    y_pred_basic_model = basic_model.predict(X_test)
-    err_sq_basic_model = np.power((y_test - y_pred_basic_model), 2).sum() / N_test
-    model_errors_test["B"].append(err_sq_basic_model)
+    model_errors_test["B"].append(evaluate_model(basic_model, X_test, y_test))
 
     # ridge regression model
     lambdas = np.logspace(-2, 2, 32)
     rr_model = RidgeRegressionModel()
     rr_model.fit(X_train, y_train, lambdas, 10)
-    y_pred_rr_model = rr_model.predict(X_test)
-    err_sq_rr_model = np.power((y_test - y_pred_rr_model), 2).sum() / N_test
-    model_errors_test["RR"].append(err_sq_rr_model)
     model_parameters["RR"].append(rr_model.lambda_opt)
-    
+    model_errors_test["RR"].append(evaluate_model(rr_model, X_test, y_test))
+
+    # ann model
+    n_hidden = [1, 16, 32] # , 64, 128, 256, 512
+    ann_model = RegressionANNModel()
+    ann_model.fit(torch.Tensor(X_train), torch.Tensor(y_train), n_hidden, 10, max_iter=3000)
+    model_errors_test["ANN"].append(evaluate_model(ann_model, torch.Tensor(X_test), torch.Tensor(y_test)).numpy())
+    model_parameters["ANN"].append(ann_model.n_hidden)
+
+
+print(f"Model errors across {K} outer folds:")
 pprint(model_errors_test)
+
+print(f"Model parameters across {K} outer folds:")
 pprint(model_parameters)
 
 # statistical comparison - setup I - paired t-test
 # N.B. Include p-values and conﬁdence intervals for the three pairwise tests
-
-def t_test_paired(zA, zB, alpha = 0.05):
-    """Paired T-test
-
-    Assumes equal sample sizes and variance
-
-    Paired, two-tailed t-test, i.e. the hypotheses are:
-    * H0: model A and B have the same perforamnce, Z = 0
-    * H1: model A and B have different performance, Z != 0
-
-    As such, the p-value indicates that the means are different in either "direction".
-    This means that the comparison B vs. RR is equivalent to RR vs. B.
-
-    Verbose implementation, clarity > fancy one-liners.
-    See ex7_2_1.py for inspiration.
-
-    TODO:
-    * report - consider these aspects
-    * is zA supposed to be the mean E_test error? Or the error for each observation?
-    In the book box 11.3.4 they write that they recommend n >= 30 samples
-    """
-
-    z = zA - zB
-    n = len(z)
-    z_hat = np.sum(z) / n
-    # sigma^2 is the variance
-    sigma_tilde_sq = np.sum((z - z_hat)**2) / n*(n-1) # TODO: why this form of the variance?
-    # sqrt(sigma^2) is the std.dev., needed for computing the Std.err. of the mean, SEM
-    sigma_hat = np.sqrt(sigma_tilde_sq) # std.dev.
-    sem = sigma_hat / np.sqrt(n)
-    
-    # p for two-tailed T-test is: P(T > |t|) = 2 * (1 - cdf(t))
-    p = 2 * (1 - st.t.cdf(np.abs( z_hat / sem ), df=n-1)) # TODO: ask TA about the df?
-    
-    # in scipy.stats, loc is the mean and scale is the std.dev.
-    CI = st.t.interval(1-alpha, df=n-1, loc=z_hat, scale=sigma_hat)  # Confidence interval
-
-    return p, CI
-
-# p, ci = t_test_paired(np.random.normal(0, 1, 10), np.random.normal(10, 1, 10)) # test
-p, ci = t_test_paired(np.array(model_errors_test["B"]), np.array(model_errors_test["RR"]))
-print(f"p = {p}, with CI: {ci}")
-
 model_combinations = list(combinations(models, 2))
 
 for mA, mB in model_combinations:
-    print(mA, mB)
+    print(f"Comparing: {mA} and {mB}")
     # 1) extract errors
+    zA = np.array(model_errors_test[mA])
+    zB = np.array(model_errors_test[mB])
 
     # 2) feed to t_test func
+    p, ci = t_test_paired(zA, zB)
+    print(f"p = {p}, with CI: {ci}")
+
+    # 3) print conclusion
+    if p < 0.05:
+        print(f"H1: models {mA} and {mB} have different performance, Z != 0")
+    else: # p >= 0.05
+        print(f"H0: models {mA} and {mB} have the same perforamnce, Z = 0")
+
