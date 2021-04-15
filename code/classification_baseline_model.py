3525e5f3eae4fe1051a4d75087ad70b4ef0e5052
tstannius <tobiasostannius@gmail.com>
2021-04-15 12:58:41 +0200

    Add baseline classification model

---
+++ code/classification_baseline_model.py
@@ -0,0 +1,32 @@
+from classification_transform_data import *
+# imports: numpy as np, pandas as pd
+# import numpy as np
+
+
+class ClassificationBaselineModel(object):
+    """A simple baseline model which simply predicts the class with highest frequency"""
+    def __init__(self):
+        self.w_star = None
+        self.opt_lambda = None
+
+    def fit(self, y):
+        # TODO: use K-fold CV?
+        # count occourences of each class
+        counts = y.sum(axis=0)
+
+        # the most common class is the one with largest count
+        class_common = np.argmax(counts)
+        
+        self.y_pred = class_common
+
+    def predict(self, X):
+        assert(self.y_pred is not None), "Model not trained yet!"
+        
+        y_pred = np.array([self.y_pred] * X.shape[0])
+        return y_pred
+
+
+# test      
+# bm = ClassificationBaselineModel()
+# bm.fit(y)
+# print(bm.predict(X))
